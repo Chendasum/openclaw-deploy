f643946b4010a22efafbdf0f39850797fd161655
Chendasum <chendasum168@gmail.com>
2026-03-24 23:45:37 +0700

    Update server.js

--- openclaw-deploy/server.js
+++ openclaw-deploy/server.js
@@ -1,1 +1,13 @@
+// server.js
+import express from "express";
 
+const app = express();
+const PORT = process.env.PORT || 18789;
+
+app.get("/", (req, res) => {
+  res.send("OpenClaw Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
